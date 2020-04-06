631ba79c6028a42d3414e093316bc5216a8127d6
Rafael Alexandre Possebon Carda <rafaelcarda@ciandt.com>
2020-04-06 17:31:56 -0300

    Adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,14 @@
-import { InMemoryDbService } from "angular-in-memory-web-api";
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDatanase implements InMemoryDbService {
     createDb(){
-        const categories =[
-            { id: 1, name: 'Moradia', descricao: 'Pagamentos de Contas da Casa' },
-            { id: 2, name: 'Saúde', descricao: 'Plano de Saúde' },
-            { id: 3, name: 'Lazer', descricao: 'Cinema, parque, praia, etc' },
-            { id: 4, name: 'Salário', descricao: 'Recebimento Salário' },
-            { id: 5, name: 'Freelas', descricao: 'Trabalhos como freelancer' }
+        const categories: Category[] = [
+            { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
+            { id: 2, name: 'Saúde', description: 'Plano de Saúde' },
+            { id: 3, name: 'Lazer', description: 'Cinema, parque, praia, etc' },
+            { id: 4, name: 'Salário', description: 'Recebimento Salário' },
+            { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer' }
         ];
 
         return { categories };
