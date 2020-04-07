64ecaba8b4524eb00295ed9c06a3185162f46c5d
Rafael Alexandre Possebon Carda <rafaelcarda@ciandt.com>
2020-04-07 13:29:43 -0300

    improving category list component

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  public categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.categoryService.getall().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar lista')
+    );
+  }
+
+  deleteCategory(category) {
+    const mustDelete = confirm('Deseja excluir esta categoria?');
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(el => el !== category),
+        () => alert('Erro na exclusão de categoria')
+      );
+    }
   }
 
 }
