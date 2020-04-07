5e6e0e2cdd8590f2142d6774c12305494f68cb71
Rafael Alexandre Possebon Carda <rafaelcarda@ciandt.com>
2020-04-07 16:47:57 -0300

    Setting up EntruesModule

---
+++ src/app/pages/entries/shared/entry.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
+import { Entry } from './entry.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EntryService {
+
+  /* No in-memory esta retornando entries */
+  private apiPath = 'api/entries';
+
+  constructor(private http: HttpClient) { }
+
+  // METHODS
+  getall(): Observable<Entry[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntries)
+    );
+  }
+
+  getById(id: number): Observable<Entry> {
+    // const url = `$(this.apiPath)/$(id)`; não sei pq não funcionou
+    const url = this.apiPath + '/' + id;
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntry)
+    );
+  }
+
+  create(entry: Entry): Observable<Entry> {
+    return this.http.post(this.apiPath, entry).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntry)
+    );
+  }
+
+  update(entry: Entry): Observable<Entry> {
+    const url = this.apiPath + '/' + entry.id;
+    // retornar a propria categoria, por que o in-memory-db, no put não retorna nada,
+    // por isso força o retorno. Mas se for um servidor real, será devolvido o objeto novo
+    return this.http.put(url, entry).pipe(
+      catchError(this.handleError),
+      map(() => entry)
+    );
+  }
+
+  delete(id: number): Observable<any> {
+    const url = this.apiPath + '/' + id;
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+  // PRIVATE METHODS
+  private handleError(error: any): Observable<any> {
+    console.log('ERRO NA REQUISIÇÃO => ', error);
+    return throwError(error);
+  }
+
+  private jsonDataToEntry(jsonData: any): Entry {
+    return jsonData as Entry;
+  }
+
+  private jsonDataToEntries(jsonData: any[]): Entry[] {
+    const entries: Entry[] = [];
+    jsonData.forEach(el => entries.push(el as Entry));
+    return entries;
+  }
+}
