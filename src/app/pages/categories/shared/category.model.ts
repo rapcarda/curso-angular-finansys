efda59af50f7589e016171207312145143f7ed1d
Rafael Alexandre Possebon Carda <rafaelcarda@ciandt.com>
2020-04-08 16:39:16 -0300

    adding BaseResourceModel

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,7 +1,12 @@
-export class Category {
+import { BaseResourceModel } from '../../../shared/models/base-resource.model';
+
+export class Category extends BaseResourceModel {
     constructor(
         public id?: number,
         public name?: string,
         public description?: string
-    ){}
+    ){
+        // indica que chama o construtor da classe base
+        super();
+    }
 }
