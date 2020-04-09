e6f105f2026c8b1cb46011acb3d8848883f3c750
Rafael Alexandre Possebon Carda <rafaelcarda@ciandt.com>
2020-04-09 17:44:31 -0300

    Creating BreadCrumbComponent

---
+++ src/app/shared/components/bread-crumb/bread-crumb.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-bread-crumb',
+  templateUrl: './bread-crumb.component.html',
+  styleUrls: ['./bread-crumb.component.css']
+})
+export class BreadCrumbComponent implements OnInit {
+
+  @Input() items: Array<BreadCrumbItem> = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  isLastItem(item: BreadCrumbItem): boolean {
+    const index = this.items.indexOf(item);
+    return index + 1 === this.items.length;
+  }
+
+}
