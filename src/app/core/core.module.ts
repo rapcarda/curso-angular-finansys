743aa6b689be4f365a40866152b344df2858bdd8
Rafael Alexandre Possebon Carda <rafaelcarda@ciandt.com>
2020-04-10 11:38:44 -0300

    Adding NavBarComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,20 +3,23 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 // Para configurar interceptação de requisições do plugin in-memory-database
 // Quando usar serviço real, deve-se remover estas chamadas, e o arquivo in-memory-database.ts
 import { HttpClientInMemoryWebApiModule, InMemoryDbService } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavBarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    RouterModule,
     // Para interceptar requisições com o in-memory-database
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
@@ -24,7 +27,10 @@ import { InMemoryDatabase } from '../in-memory-database';
     // shared modules
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+
+    // Shared Components
+    NavBarComponent
   ]
 })
 export class CoreModule { }
